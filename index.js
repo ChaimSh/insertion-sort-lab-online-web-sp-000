61a81114988a052b41199593c11698057e77031a
Kirill Chaim Shcherbina <rabbichaim770@gmail.com>
2020-08-07 03:29:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // function findMinAndRemove(array){
-//   let min
+//   let min = array[0]
 //   let minIndex = 0
-//   for (let i = 0; i < array.length - 1; i++) {
+//   for (let i = 0; i < array.length; i++) {
 //     if (array[i] < array[0]){
 //       array[0] = array[i]
 //       min = array[0]
