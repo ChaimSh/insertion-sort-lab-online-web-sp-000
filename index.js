4d72dc5fa97c631a415663318e511fbb7f2bf2e6
Kirill Chaim Shcherbina <rabbichaim770@gmail.com>
2020-08-07 03:27:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 function findMinAndRemove(array){
   let min
+  let minIndex = 0
   for (let i = 0; i < array.length - 1; i++) {
     if (array[i] < array[0]){
       array[0] = array[i]
       min = array[0]
+      minIndex = i
     }
   }
+  array.splice(minIndex, 1);
+  return min
 }
 
 function selectionSort(array){
