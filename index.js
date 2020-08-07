a011f9de6a739f123bce849652a9061e58e37366
Kirill Chaim Shcherbina <rabbichaim770@gmail.com>
2020-08-07 03:23:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function findMinAndRemove(array){
   let min = []
-  for (let i = 1; i < array.length - 1; i++) {
+  for (let i = 0; i < array.length - 1; i++) {
     if (array[i] < array[0]){
       array[0] = array[i]
       min = array[0]
