8032b220ba3a6cec3507b065b1e94edeeb32e926
Kirill Chaim Shcherbina <rabbichaim770@gmail.com>
2020-08-07 03:29:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-// function findMinAndRemove(array){
-//   let min = array[0]
-//   let minIndex = 0
-//   for (let i = 0; i < array.length; i++) {
-//     if (array[i] < array[0]){
-//       array[0] = array[i]
-//       min = array[0]
-//       minIndex = i
-//     }
-//   }
-//   array.splice(minIndex, 1);
-//   return min
-// }
-
 function findMinAndRemove(array){
-  let currentMin = array[0]
+  let min = array[0]
   let minIndex = 0
-  for(let i = 0; i < array.length; i++){
-    if(array[i] < currentMin){
-      currentMin = array[i]
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < array[0]){
+      array[0] = array[i]
+      min = array[0]
       minIndex = i
     }
   }
   array.splice(minIndex, 1);
-  return currentMin;
+  return min
 }
+
+// function findMinAndRemove(array){
+//   let currentMin = array[0]
+//   let minIndex = 0
+//   for(let i = 0; i < array.length; i++){
+//     if(array[i] < currentMin){
+//       currentMin = array[i]
+//       minIndex = i
+//     }
+//   }
+//   array.splice(minIndex, 1);
+//   return currentMin;
+// }
 
 function selectionSort(array){
 
